c0422f40ba779d07885115685f557da4d6da5a59
이원민 <powerjade@hivelab.co.kr>
2022-08-11 15:57:18 +0900

    initial commit!

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,19 @@
+<svelte:head>
+  <title>스벨트 스터-디</title>
+  <link
+    rel="stylesheet"
+    as="style"
+    href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.5/dist/web/static/pretendard.css"
+    crossorigin="true"
+  />
+</svelte:head>
+
+<slot />
+
+<style global>
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Pretendard';
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,38 @@
+<script context="module">
+  export const prerender = true;
+</script>
+
+<script lang="ts">
+  let y: number = 0;
+
+  $: {
+    console.log(y);
+  }
+</script>
+
+<svelte:window bind:scrollY={y} />
+
+<div class="wrap">
+  <div class="sticky">
+    <h1>대세는 스벨트일까?</h1>
+    <p>scrollY: {y}</p>
+  </div>
+
+  {#each Array(100) as _, i}
+    {i}<br />
+  {/each}
+</div>
+
+<style>
+  .wrap {
+    padding: 0 16px;
+  }
+  .sticky {
+    padding: 16px 0;
+    position: sticky;
+    top: 0;
+  }
+  .sticky h1 {
+    margin: 0;
+  }
+</style>
